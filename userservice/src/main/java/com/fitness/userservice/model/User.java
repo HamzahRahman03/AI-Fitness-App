af071926b28e2ccc152a4d2fb84be4d8e54f57e5
Syed Hamzah Rahman <syedhamzahrahman@Hamzahs-MacBook-Air.local>
2026-01-17 19:42:39 +0530

    first commit

---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,31 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @CreationTimestamp
+    private String createdAt;
+
+    @UpdateTimestamp
+    private String updatedAt;
+
+}
