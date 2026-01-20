f55fa2113c0fd5befe2910ad4672392149f367c0
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-01-20 21:34:48 +0530

    20012026

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.fitness.userservice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users}")
 public class UserController {
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(userService.register(request));
+    }
 
 }
