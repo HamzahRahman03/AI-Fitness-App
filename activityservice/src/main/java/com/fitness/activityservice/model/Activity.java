b114fe4d9dc7e87b23f6f549f3b591a27cc01216
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-01-29 22:48:23 +0530

    29012026

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,5 +1,22 @@
 package com.fitness.activityservice.model;
 
-public class Activity  {
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Document()
+@Data
+public class Activity {
+    private String id;
+    private String userId;
+    private LocalDateTime startTime;
+    private Integer duration;
+    private Integer caloriesBurnt;
+    private ActivityType type;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private Map<String, Object> additionalMetrics;
 }
-s
---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,11 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    WALKING,
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    HIIT,
+    BOXING,
+    OTHER
+}
