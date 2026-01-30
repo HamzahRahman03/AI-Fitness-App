262d083b284c9d539f5e1eeebe7ae4b323d8c9c6
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-01-30 23:20:45 +0530

    30012026

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,13 +1,19 @@
 package com.fitness.activityservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
-@Document()
+@Document(collection = "activities")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
     private String id;
     private String userId;
