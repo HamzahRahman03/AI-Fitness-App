31e2160e157526d6499c21a5790d1c334205f6ba
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-02-02 22:20:27 +0530

    02022026_2

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -6,10 +6,9 @@ import com.fitness.activityservice.service.ActivityService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
@@ -18,8 +17,18 @@ public class ActivityController {
 
     private ActivityService activityService;
 
-    @PostMapping("/createActivity")
+    @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
         return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping("/{userId} ")
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@PathVariable String userId){
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-User-ID") String userId){
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
     }
 }
