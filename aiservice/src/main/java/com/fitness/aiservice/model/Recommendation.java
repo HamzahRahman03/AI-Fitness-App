3d116e85d99598b9be550f4649b8baeb3f89cdd8
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-02-10 21:22:48 +0530

    Database connected with aiservice

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -0,0 +1,38 @@
+package com.fitness.aiservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "ai_recommendations")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recommendation {
+
+    @Id
+    private String id;
+    private String activityId;
+    private String userId;
+    private String activityType;
+    private String recommendation;
+
+    @CreatedDate
+    private String createdAt;
+
+    @LastModifiedDate
+    private String updatedAt;
+
+    private List<String> safetyMeasures;
+    private List<String> suggestions;
+    private List<String> improvements;
+
+}
