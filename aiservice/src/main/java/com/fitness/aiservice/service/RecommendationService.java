c1b37b38554565313d2a43357671cc1235402f09
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-02-12 19:52:27 +0530

    API endpoints defined for aiservice

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,26 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.respository.RecommendationRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getRecommendationsForUser(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getRecommendationsForActivity(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() -> new RuntimeException("No Activity Found for activityId : " + activityId));
+    }
+}
