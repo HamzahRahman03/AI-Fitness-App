b06bd18ea83c41b5d771403691d469e6e566268b
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-03-09 19:55:26 +0530

    redux store set up and log in button created

--- fitness-app-client/src/App.jsx
+++ fitness-app-client/src/App.jsx
@@ -1,8 +1,10 @@
-
+import { Button } from "@mui/material";
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
 
 function App() {
   return (
     <>
+    <Button variant="contained" color="#3b8093">Login</Button>
     </>
   )
 }
