1efe45a7c591863f5d5ddd58197306ef5dc12eae
Syed Hamzah Rahman <syedhamzahrahman@gmail.com>
2026-03-09 22:18:54 +0530

    Login button redirecting to Keycloak login page

--- fitness-app-client/src/App.jsx
+++ fitness-app-client/src/App.jsx
@@ -1,10 +1,28 @@
 import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
+import { setCredentials } from "./store/authSlice";
 
 function App() {
+  const { token, tokenData, logIn, logOut, isAuthenticated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if(token){
+      dispatch(setCredentials({token, user: tokenData}));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
   return (
     <>
-    <Button variant="contained" color="#3b8093">Login</Button>
+    <Button variant="contained" color="#3b8093"
+            onClick={() => {
+              logIn();
+            }}>Login</Button>
     </>
   )
 }
